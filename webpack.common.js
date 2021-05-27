44887a95470cbb23d263ae0b5f76b6380665234d
Regina Wang <rrreggginnna@gmail.com>
2021-05-26 17:33:38 -0700

    init ts react, ty CL and @BryanPan342 for starters!

---
+++ webpack.common.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    index: './src/index.tsx',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-typescript',
+            ],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.jsx', '.js' ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html', // TODO: YOINK TLA LOGO
+      favicon: './public/favicon.svg',
+      filename: './index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
