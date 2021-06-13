b2c90a8b182ed2acb9548a473d303352515d3ea8
Regina Wang <rrreggginnna@gmail.com>
2021-06-13 01:40:35 -0700

    add lint hook pre-push

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,7 +5,7 @@ import '../styles/App.scss';
 function App() : JSX.Element {
   return (
     <div className="app">
-      <header className="app-header">
+      <header     className="app-header">
         <img src={logo} className="app-logo" alt="logo" />
         <p>
           Edit
