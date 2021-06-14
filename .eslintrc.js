2f773ac71656f6f2884b149f076144d590abfe20
Regina Wang <rrreggginnna@gmail.com>
2021-06-14 01:13:12 -0700

    linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,48 +4,48 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:import/typescript",
-    "plugin:@typescript-eslint/recommended",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/recommended',
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: "module",
-    project: "./tsconfig.json",
+    sourceType: 'module',
+    project: './tsconfig.json',
   },
-  plugins: ["import", "react", "@typescript-eslint"],
+  plugins: ['import', 'react', '@typescript-eslint'],
   rules: {
     // A few more opinions in addition to extensions
 
     // As per React 17 changes! https://reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html
-    "react/react-in-jsx-scope": "off",
-    "react/jsx-uses-react": "off",
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-react': 'off',
 
-    "linebreak-style": ["error", "unix"],
+    'linebreak-style': ['error', 'unix'],
 
-    "@typescript-eslint/no-require-imports": ["error"],
+    '@typescript-eslint/no-require-imports': ['error'],
 
-    "@typescript-eslint/no-non-null-assertion": "off",
+    '@typescript-eslint/no-non-null-assertion': 'off',
 
-    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
 
     // 2 space indentation
-    "@typescript-eslint/indent": ["error", 2],
+    '@typescript-eslint/indent': ['error', 2],
 
     // Style
-    quotes: ["error", "single", { avoidEscape: true }],
+    quotes: ['error', 'single', { avoidEscape: true }],
 
     // ensures clean diffs, see https://medium.com/@nikgraf/why-you-should-enforce-dangling-commas-for-multiline-statements-d034c98e36f8
-    "comma-dangle": ["error", "always-multiline"],
+    'comma-dangle': ['error', 'always-multiline'],
 
     // Require all imported dependencies are actually declared in package.json
-    "import/no-extraneous-dependencies": [
-      "error",
+    'import/no-extraneous-dependencies': [
+      'error',
       {
         optionalDependencies: false, // Disallow importing optional dependencies (those shouldn't be in use in the project)
         peerDependencies: false, // Disallow importing peer dependencies (that aren't also direct dependencies)
@@ -53,38 +53,38 @@ module.exports = {
     ],
 
     // Require all imported libraries actually resolve (!!required for import/no-extraneous-dependencies to work!!)
-    "import/no-unresolved": ["error"],
+    'import/no-unresolved': ['error'],
 
     // Require an ordering on all imports
-    "import/order": [
-      "warn",
+    'import/order': [
+      'warn',
       {
-        groups: ["builtin", "external"],
-        alphabetize: { order: "asc", caseInsensitive: true },
+        groups: ['builtin', 'external'],
+        alphabetize: { order: 'asc', caseInsensitive: true },
       },
     ],
 
     // Cannot import from the same module twice
-    "no-duplicate-imports": ["error"],
+    'no-duplicate-imports': ['error'],
 
     // Cannot shadow names
-    "@typescript-eslint/no-shadow": ["error"],
+    '@typescript-eslint/no-shadow': ['error'],
 
     // Required spacing in property declarations (copied from TSLint, defaults are good)
-    "key-spacing": ["error"],
+    'key-spacing': ['error'],
 
     // Require semicolons
-    semi: ["error", "always"],
+    semi: ['error', 'always'],
 
     // Don't unnecessarily quote properties
-    "quote-props": ["error", "consistent-as-needed"],
+    'quote-props': ['error', 'consistent-as-needed'],
 
     // No multiple empty lines
-    "no-multiple-empty-lines": ["error"],
+    'no-multiple-empty-lines': ['error'],
 
     // Max line lengths
-    "max-len": [
-      "error",
+    'max-len': [
+      'error',
       {
         code: 120,
         ignoreUrls: true, // Most common reason to disable it
@@ -95,39 +95,39 @@ module.exports = {
     ],
 
     // One of the easiest mistakes to make
-    "@typescript-eslint/no-floating-promises": ["error"],
+    '@typescript-eslint/no-floating-promises': ['error'],
 
     // Don't leave log statements littering the premises!
-    "no-console": ["error"],
+    'no-console': ['error'],
 
     // Useless diff results
-    "no-trailing-spaces": ["error"],
+    'no-trailing-spaces': ['error'],
 
     // Must use foo.bar instead of foo['bar'] if possible
-    "dot-notation": ["error"],
+    'dot-notation': ['error'],
 
     // Are you sure | is not a typo for || ?
-    "no-bitwise": ["error"],
+    'no-bitwise': ['error'],
 
     // Member ordering
-    "@typescript-eslint/member-ordering": [
-      "error",
+    '@typescript-eslint/member-ordering': [
+      'error',
       {
         default: [
-          "public-static-field",
-          "public-static-method",
-          "protected-static-field",
-          "protected-static-method",
-          "private-static-field",
-          "private-static-method",
+          'public-static-field',
+          'public-static-method',
+          'protected-static-field',
+          'protected-static-method',
+          'private-static-field',
+          'private-static-method',
 
-          "field",
+          'field',
 
           // Constructors
-          "constructor", // = ["public-constructor", "protected-constructor", "private-constructor"]
+          'constructor', // = ['public-constructor', 'protected-constructor', 'private-constructor']
 
           // Methods
-          "method",
+          'method',
         ],
       },
     ],
