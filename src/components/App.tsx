5e59478f573913b4127e7f3e8b4d7eab25fa737a
Regina Wang <rrreggginnna@gmail.com>
2022-01-25 14:15:21 -0800

    remove lint error
    Signed-off-by: Regina Wang <rrreggginnna@gmail.com>

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,6 @@
 import AppWrapper from './shared/AppWrapper';
 import { HeaderSections } from './shared/globalTypes';
-require '../assets/WestwoodSans-Regular.ttf';
+import '../assets/WestwoodSans-Regular.ttf';
 
 function App(): JSX.Element {
   return (
